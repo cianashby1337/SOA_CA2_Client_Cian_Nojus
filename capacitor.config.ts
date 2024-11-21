17fcacfa92d24183a71b2e328a0cb419adb162a2
Cian Ashby <D00241714@student.dkit.ie>
2024-11-21 11:51:14 +0000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'SOA_CA2_Client_Cian_Nojus',
+  webDir: 'dist'
+};
+
+export default config;
