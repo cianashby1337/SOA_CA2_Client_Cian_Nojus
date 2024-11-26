12fcffd2d3a90a1b0d569b3ed6c5e57687d8b672
Cian Ashby <D00241714@student.dkit.ie>
2024-11-26 02:20:50 +0000

    Add key and wrapper for Google OAuth - An ignored .env file holds the key. Create this file with your own key

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,11 @@
 /// <reference types="vite/client" />
+
+// The code below was used to set up environment variables, it
+// was retrieved from https://vite.dev/guide/env-and-mode.html
+interface ImportMetaEnv {
+	readonly VITE_GOOGLE_LOGIN: string
+  }
+  
+  interface ImportMeta {
+	readonly env: ImportMetaEnv
+  }
