338ca8c9966e6cd68cdd2e1a0174cd07804392b5
Cian Ashby <D00241714@student.dkit.ie>
2024-11-26 02:44:22 +0000

    Log decoded JWT from Google OAuth to console

--- src/pages/Tab2.tsx
+++ src/pages/Tab2.tsx
@@ -4,6 +4,17 @@ import './Tab2.css';
 
 import { GoogleLogin } from '@react-oauth/google';
 
+// Function to decode the response was taken from wbq's answer at https://stackoverflow.com/questions/68524360/how-can-i-decode-a-google-oauth-2-0-jwt-credential-token
+function decodeJwtResponse(token:string) {
+  var base64Url = token.split('.')[1];
+  var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+  var jsonPayload = decodeURIComponent(window.atob(base64).split('').map(function(c) {
+    return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+  }).join(''));
+
+  return JSON.parse(jsonPayload);
+}
+
 const Tab2: React.FC = () => {
   return (
     <IonPage>
@@ -22,9 +33,13 @@ const Tab2: React.FC = () => {
 
 
         <GoogleLogin
-        // This GoogleLogin element was taken from the guide of it's package: https://www.npmjs.com/package/@react-oauth/google
+        // This GoogleLogin element was constructed from the guide of it's package: https://www.npmjs.com/package/@react-oauth/google
+        // In addition, the JWT response decoding takes code from Ali Zulfikar's answer at https://stackoverflow.com/questions/68524360/how-can-i-decode-a-google-oauth-2-0-jwt-credential-token
           onSuccess={credentialResponse => {
-            console.log(credentialResponse);
+            if(credentialResponse.credential != null) {
+              const USER_CREDENTIAL = decodeJwtResponse(credentialResponse.credential);
+              console.log(USER_CREDENTIAL);
+             }
           }}
           onError={() => {
             console.log('Login Failed');
