fc5de35ebd30e2e366c09b2feb503216d6a5afe7
Cian Ashby <57039577+cianashby1337@users.noreply.github.com>
2024-11-28 23:27:49 +0000

    Restrict release year to an integer value only

--- src/pages/AddGame/AddGameForm.tsx
+++ src/pages/AddGame/AddGameForm.tsx
@@ -1,16 +1,33 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonInput, IonList } from '@ionic/react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonInput, IonList, IonSelect } from '@ionic/react';
 import ExploreContainer from '../../components/ExploreContainer';
 
 import './AddGameForm.css';
 
+const currentYear = Date.now.toString();
+console.log(currentYear)
+
 const AddGameForm: React.FC = () => {
-	// IonList and IonInput were retrieved from the official Ionic docs: https://ionicframework.com/docs/api/input
+	// IonList and IonInput were referenced from the official Ionic docs: https://ionicframework.com/docs/api/input
   return (
 		<>
 			<IonList id="addGameForm">
-				<IonInput placeholder='Game Title'/>
-				<IonInput placeholder='Genre'/>
-				<IonInput placeholder='Release Year'/>
+				<div className="inputRow">
+					<p>Game Title:</p>
+					<IonInput className="addGameFormItem"/>
+				</div>
+				<div className="inputRow">
+					<p>Genre:</p>
+					<IonInput className="addGameFormItem"/>
+				</div>
+				<div className="inputRow">
+					<p>Release Year:</p>
+					<IonInput type="number"  className="addGameFormItem"/>
+				</div>
+				<div className="inputRow">
+					<p>Game Title:</p>
+					<IonSelect className="addGameFormItem">
+					</IonSelect>
+				</div>
 			</IonList>
 		</>
   );
