cecd8810c630e505127e31ecd6e2df3e674af72a
Cian Ashby <57039577+cianashby1337@users.noreply.github.com>
2024-11-28 22:51:41 +0000

    Add basic styling to form list

--- src/pages/AddGame/AddGameForm.tsx
+++ src/pages/AddGame/AddGameForm.tsx
@@ -1,11 +1,13 @@
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonInput, IonList } from '@ionic/react';
 import ExploreContainer from '../../components/ExploreContainer';
 
+import './AddGameForm.css';
+
 const AddGameForm: React.FC = () => {
 	// IonList and IonInput were retrieved from the official Ionic docs: https://ionicframework.com/docs/api/input
   return (
 		<>
-			<IonList>
+			<IonList id="addGameForm">
 				<IonInput placeholder='Game Title'/>
 				<IonInput placeholder='Genre'/>
 				<IonInput placeholder='Release Year'/>
