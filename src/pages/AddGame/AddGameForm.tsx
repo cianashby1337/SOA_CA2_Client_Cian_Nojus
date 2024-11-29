0b7442de09014d639a5a3f13579c14ef33b8fe98
Cian Ashby <57039577+cianashby1337@users.noreply.github.com>
2024-11-29 21:08:52 +0000

    Convert year to IonDatetime wheel

--- src/pages/AddGame/AddGameForm.tsx
+++ src/pages/AddGame/AddGameForm.tsx
@@ -1,4 +1,4 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonInput, IonList, IonSelect } from '@ionic/react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonInput, IonList, IonSelect, IonDatetime } from '@ionic/react';
 import ExploreContainer from '../../components/ExploreContainer';
 
 import './AddGameForm.css';
@@ -8,24 +8,25 @@ console.log(currentYear)
 
 const AddGameForm: React.FC = () => {
 	// IonList and IonInput were referenced from the official Ionic docs: https://ionicframework.com/docs/api/input
+	// IonDatetime was referenced from https://ionicframework.com/docs/api/datetime#wheel-style-pickers
   return (
 		<>
 			<IonList id="addGameForm">
 				<div className="inputRow">
 					<p>Game Title:</p>
-					<IonInput className="addGameFormItem"/>
+					<IonInput className="addGameFormInput"/>
 				</div>
 				<div className="inputRow">
 					<p>Genre:</p>
-					<IonInput className="addGameFormItem"/>
+					<IonInput className="addGameFormInput"/>
 				</div>
 				<div className="inputRow">
 					<p>Release Year:</p>
-					<IonInput type="number"  className="addGameFormItem"/>
+					<IonDatetime presentation="year" preferWheel={true} className="addGameFormWheel"/>
 				</div>
 				<div className="inputRow">
 					<p>Game Title:</p>
-					<IonSelect className="addGameFormItem">
+					<IonSelect className="addGameFormInput">
 					</IonSelect>
 				</div>
 			</IonList>
