adfd7e236f276fad383d6872afcc634084c9b2c3
Cian Ashby <D00241714@student.dkit.ie>
2024-12-05 15:55:08 +0000

    Remove redundant code in setupTests.ts

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -4,9 +4,8 @@
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom/extend-expect';
 
-import { setupIonicReact } from '@ionic/react';
-
-setupIonicReact();
+// The command to run tests was found by trying and failing to run tests through the reference below, 
+// and then running npm run out of desparation, which gave the suggestion test.unit: https://ionic.io/blog/testing-ionic-react-apps-with-jest-and-react-testing-library
 
 // Mock matchmedia
 window.matchMedia = window.matchMedia || function() {
